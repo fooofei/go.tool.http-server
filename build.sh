39bf8b4c37c7ec5cc4fbe00a60d19b6d90c0d1dd
fooofei <aihujianfei@qq.com>
2020-09-06 21:55:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+cur=$(dirname "$(readlink -f $0)")
+
 bin=sim_http_server
 
-rm -f $bin
-rm -f $bin.linux
-
-GOOS=linux GOARCH=amd64 go build -mod=vendor -v
-mv $bin $bin.linux
-go build -mod=vendor -v
+GOOS=linux GOARCH=arm64 go build -mod=vendor -v -o $bin.linux.arm64 $cur/cmd/server
+GOOS=linux GOARCH=amd64 go build -mod=vendor -v -o $bin.linux.amd64 $cur/cmd/server
+GOOS=windows GOARCH=amd64 go build -mod=vendor -v -o $bin.exe $cur/cmd/server
